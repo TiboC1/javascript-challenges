40ecca33173d78439976a2383510e3aab7d2d151
Tibo Colman <tibocolman@gmail.com>
2019-11-09 20:14:16 +0100

    04 and 05 done

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -17,5 +17,63 @@ let target = document.getElementById("target"),
     buttonThree = document.getElementById("part-three"),
     buttonFour = document.getElementById("part-four");
 
+    let one = 460
+    let two = 0
+    let three = 0
+    let four = 0
+
+    buttonOne.value = one;
+    buttonTwo.value = ("0" + two).slice(-2);
+    buttonThree.value = ("0" + three).slice(-2);
+    buttonFour.value = ("0" + four).slice(-2);
+
+
+    function update(){
+        target.innerHTML = "+" + buttonOne.value + buttonTwo.value + buttonThree.value + buttonFour.value
+        buttonOne.innerHTML = buttonOne.value
+        buttonTwo.innerHTML = buttonTwo.value
+        buttonThree.innerHTML = buttonThree.value
+        buttonFour.innerHTML = buttonFour.value
+    }
+
+    buttonOne.addEventListener("click", function(){
+        if (one < 499){
+            one += 1
+        } else {
+            one = 460
+        }
+        buttonOne.value = one;
+        update()
+    })
+
+    buttonTwo.addEventListener("click", function(){
+        if(two < 100){
+        two++
+        } else {
+        two = 0
+        }
+        buttonTwo.value = ("0" + two).slice(-2);
+        update()
+    })
+    
+    buttonThree.addEventListener("click", function(){
+        if(two < 100){
+            three++
+        } else {
+            two = 0
+        }        
+        buttonThree.value = ("0" + three).slice(-2);
+        update()
+    })
+    
+    buttonFour.addEventListener("click", function(){
+        if(two < 100){
+            four++
+        } else {
+            two = 0
+        }        
+        buttonFour.value = ("0" + four).slice(-2);
+        update()
+    })
     
 })();
--- 07-misc/05-worst-ui-three/script.js
+++ 07-misc/05-worst-ui-three/script.js
@@ -11,6 +11,61 @@
 
 (function() {
 
-    // your code here
+    let target = document.getElementById("target"),
+    inputOne = document.getElementById("part-one"),
+    inputTwo = document.getElementById("part-two"),
+    inputThree = document.getElementById("part-three"),
+    inputFour = document.getElementById("part-four"),
+    buttonOne = document.getElementById("fix-part-one"),
+    buttonTwo = document.getElementById("fix-part-two"),
+    buttonThree = document.getElementById("fix-part-three"),
+    buttonFour = document.getElementById("fix-part-four");
 
+    let one = 460
+    let two = 0
+    let three = 0
+    let four = 0
+    
+    inputOne.value = one;
+    inputTwo.value = ("0" + two).slice(-2);
+    inputThree.value = ("0" + three).slice(-2);
+    inputFour.value = ("0" + four).slice(-2);
+
+
+    function update(){
+        target.innerHTML = "+" + inputOne.value + inputTwo.value + inputThree.value + inputFour.value
+        inputOne.innerHTML = inputOne.value
+        inputTwo.innerHTML = inputTwo.value
+        inputThree.innerHTML = inputThree.value
+        inputFour.innerHTML = inputFour.value
+    }
+
+    buttonOne.addEventListener("click", function(){
+
+        one = Math.floor(Math.random()*39+460)
+        inputOne.value = one
+        update()
+    })
+
+    buttonTwo.addEventListener("click", function(){
+
+        two = Math.floor(Math.random()*100)
+        inputTwo.value = ("0" + two).slice(-2);
+        update()
+    })
+    
+    buttonThree.addEventListener("click", function(){
+
+        three = Math.floor(Math.random()*100)
+        inputThree.value = ("0" + three).slice(-2);
+        update()
+    })
+    
+    buttonFour.addEventListener("click", function(){
+
+        four = Math.floor(Math.random()*100)
+        inputFour.value = ("0" + four).slice(-2);
+        update()
+    })
+    
 })();
